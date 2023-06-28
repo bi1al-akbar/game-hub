371b3284d2144ec967469e909a44eac0c418df79
Bilal Akbar <geo.bilal7@gmail.com>
2023-06-28 11:49:32 +0500

    Create a custom hook to fetch the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import apiClient, { CanceledError } from "../services/api-client";
+
+interface Game {
+	id: number;
+	name: string;
+}
+interface FetchGameResponse {
+	count: number;
+	results: Game[];
+}
+
+const useGames = () => {
+	const abortController = new AbortController();
+
+	const [games, setGames] = useState<Game[]>([]);
+	const [error, setError] = useState("");
+	useEffect(() => {
+		apiClient
+			.get<FetchGameResponse>("/games", { signal: abortController.signal })
+			.then((res) => {
+				setGames(res.data.results);
+			})
+			.catch((error) => {
+				if (error instanceof CanceledError) return;
+				setError(error.message);
+			});
+
+		return () => {
+			abortController.abort();
+		};
+	}, []);
+	return { games, error };
+};
+
+export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {CanceledError} from "axios";
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
@@ -6,3 +6,5 @@ export default axios.create({
 		key: "8b9e3196ad894f56899fec176db18dbb",
 	},
 });
+
+export {CanceledError};
