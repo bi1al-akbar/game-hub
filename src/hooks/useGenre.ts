b8942198b1571d6c652bd5fddd1b08cb7366eee5
Bilal Akbar <geo.bilal7@gmail.com>
2023-07-04 17:52:07 +0500

    Add genres as static data

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,11 +1,16 @@
-import useData from "./useData";
-
+import genres from "../data/genres.ts"
 export interface Genre {
 	id: number;
 	name: string;
-	image_background: string;	
+	image_background: string;
 }
 
-const useGenre = () => useData<Genre>("/genres");
+const useGenre = () => {
+	return {
+		data: genres,
+		isLoading: false,
+		error: null,
+	};
+};
 
 export default useGenre;
